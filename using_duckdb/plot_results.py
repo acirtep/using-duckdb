fd1c6036c4e02dc0d9ff4e8680c307352ae91df9
acirtep <acirtep>
2025-06-20 09:51:39 +0200

    fix: show only last version of repo

--- using_duckdb/plot_results.py
+++ using_duckdb/plot_results.py
@@ -5,9 +5,7 @@ from helpers import get_duckdb_conn
 def write_plot():
     duckdb_conn = get_duckdb_conn()
     px.scatter(
-        duckdb_conn.read_csv(
-            "./git_log.txt",
-        )
+        duckdb_conn.read_csv("./git_log.txt", delimiter="|", strict_mode=False, ignore_errors=True, null_padding=True)
         .select("""
             #2 as repo,
             #3 as topics,
@@ -18,12 +16,10 @@ def write_plot():
             #8 as updated_at,
             if(stars + forks + open_issues = 0, 0, log(stars + forks + open_issues)) as log_activity_count,
             substr(repo, position('[' in repo) + 1 , position(']' in repo) - 2) as repo_name,
-            count(distinct updated_at::date) 
-                over 
-            (partition by repo_name order by updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW ) as number_of_updates,
-            row_number() over (partition by repo_name, updated_at::date order by updated_at desc) as rn
+            count(distinct updated_at::date) over (partition by repo_name) as number_of_updates,
+            row_number() over (partition by repo_name order by updated_at desc) as rn
         """)
-        .filter("rn = 1 and updated_at >= now() - interval '2 weeks'")
+        .filter("rn = 1")
         .order("updated_at"),
         x="updated_at",
         y="log_activity_count",

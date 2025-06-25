195286da3f196e86cca456f6c5ae539c32c2d4a9
acirtep <acirtep>
2025-06-25 09:36:32 +0200

    fix: add scope and readme

--- using_duckdb/search_repositories.py
+++ using_duckdb/search_repositories.py
@@ -35,7 +35,7 @@ def get_search_requirements(duckdb_conn):
 
     for page in range(2, number_pages + 1):
         logger.info(f"Fetching {page} out of {number_pages}")
-        (duckdb_conn.read_json(f"{api_url}&per_page=100&page={page}")).insert_into("github_raw_data")
+        duckdb_conn.read_json(f"{api_url}&per_page=100&page={page}").insert_into("github_raw_data")
 
 
 def export_to_md(duckdb_conn):
@@ -113,10 +113,62 @@ def export_to_md(duckdb_conn):
     ).to_csv("./exported_records.md", sep="|", header=False)
 
 
+def export_to_readme(duckdb_conn):
+    selected_data = (
+        (
+            duckdb_conn.table("github_raw_data")
+            .select("unnest(items, recursive := true)")
+            .select("""
+            concat_ws(
+                '<br>',
+                concat('[', name, '](', concat('https://github.com/', full_name),')'),
+                coalesce(description, ' '),
+                concat('**License** ', coalesce(name_1, 'unknown')),
+                concat('**Owner** ', login)
+            ) as repo_details,
+            coalesce(topics, '[]')::varchar as topics,
+            coalesce(stargazers_count, 0) as stars,
+            coalesce(open_issues_count, 0) as open_issues,
+            coalesce(forks, 0) as forks,
+            created_at,
+            updated_at,
+            login,
+            coalesce(fork, false) as fork,
+            stars + open_issues + forks as activity_count
+        """)
+            .filter("""
+            login != 'duckdb'
+            and not fork
+            and activity_count >= 3
+        """)
+            .order("""
+            created_at desc,
+            updated_at desc,
+            stars desc
+        """)
+        )
+        .select("""
+            concat(
+                '|',
+                concat_ws('|', repo_details, topics, stars, open_issues, forks, created_at, updated_at)
+            ) as line
+        """)
+        .string_agg("line", sep="|\n")
+        .fetchone()[0]
+    )
+
+    with open("README.md", "w") as readme_file:
+        readme_file.write("# Repositories using `duckdb`\n")
+        readme_file.write("|Name|Topics|Stars|Open Issues|Forks|Created At|Updated At|\n")
+        readme_file.write(f"{duckdb_conn.sql("select concat(repeat('|--', 7),'|')").fetchone()[0]}\n")
+        readme_file.write(f"{selected_data}|")
+
+
 def main():
     duckdb_conn = get_duckdb_conn()
     get_search_requirements(duckdb_conn)
     export_to_md(duckdb_conn)
+    # export_to_readme(duckdb_conn)
 
 
 if __name__ == "__main__":
